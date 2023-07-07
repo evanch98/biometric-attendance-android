46f8731fb0f2a8ee95913d66b8241ae8972a98dc
Kyaw Thu <evanch98@gmail.com>
2023-07-07 23:10:31 +0800

    update the MainActivity

--- app/src/main/java/com/example/assignment_unit_4/MainActivity.kt
+++ app/src/main/java/com/example/assignment_unit_4/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.assignment_unit_4.auth.SignUp
 import com.example.assignment_unit_4.ui.theme.Assignment_unit_4Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +20,9 @@ class MainActivity : ComponentActivity() {
       Assignment_unit_4Theme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          SignUp()
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  Assignment_unit_4Theme {
-    Greeting("Android")
   }
 }

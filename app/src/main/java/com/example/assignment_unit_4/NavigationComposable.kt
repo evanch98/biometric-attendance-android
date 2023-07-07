404bcb61ed15f65755b62bb36b686a208322aedf
Kyaw Thu <evanch98@gmail.com>
2023-07-08 00:34:16 +0800

    add navigations

--- app/src/main/java/com/example/assignment_unit_4/NavigationComposable.kt
+++ app/src/main/java/com/example/assignment_unit_4/NavigationComposable.kt
@@ -4,12 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.assignment_unit_4.auth.Login
+import com.example.assignment_unit_4.auth.SignUp
 
 @Composable
 fun Navigation(navController: NavHostController) {
   NavHost(navController = navController, startDestination = SignUp.route) {
     composable(SignUp.route) {
-
+      SignUp(navController = navController)
+    }
+    composable(Login.route) {
+      Login(navController = navController)
     }
   }
 }

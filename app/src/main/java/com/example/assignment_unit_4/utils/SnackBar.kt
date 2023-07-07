155c58e444df36a1306186f34e892c0a3ea311ae
Kyaw Thu <evanch98@gmail.com>
2023-07-08 00:46:42 +0800

    update the SnackBar.kt file

--- app/src/main/java/com/example/assignment_unit_4/utils/SnackBar.kt
+++ app/src/main/java/com/example/assignment_unit_4/utils/SnackBar.kt
@@ -1,2 +1,21 @@
 package com.example.assignment_unit_4.utils
 
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+
+@Composable
+fun SnackBar(
+  message: String,
+  duration: SnackbarDuration = SnackbarDuration.Short,
+) {
+  val snackBarHostState = remember { SnackbarHostState() }
+
+  LaunchedEffect(snackBarHostState) {
+    snackBarHostState.showSnackbar(message, duration = duration)
+  }
+  SnackbarHost(hostState = snackBarHostState)
+}

83828b32dd6a9a729a293b4efcf3ccf59617bcfb
Kyaw Thu <evanch98@gmail.com>
2023-07-08 00:12:45 +0800

    update the Destinations

--- app/src/main/java/com/example/assignment_unit_4/Destinations.kt
+++ app/src/main/java/com/example/assignment_unit_4/Destinations.kt
@@ -1,2 +1,13 @@
 package com.example.assignment_unit_4
 
+interface Destinations {
+  val route: String
+}
+
+object SingUp: Destinations {
+  override val route: String = "SignUp"
+}
+
+object Login: Destinations {
+  override val route: String = "Login"
+}

b7a3a9f7e13226d1308e7c7b3f199cd7b5cfcc17
Kyaw Thu <evanch98@gmail.com>
2023-07-08 00:01:06 +0800

    update the SignUp composable

--- app/src/main/java/com/example/assignment_unit_4/auth/SignUp.kt
+++ app/src/main/java/com/example/assignment_unit_4/auth/SignUp.kt
@@ -1,19 +1,23 @@
 package com.example.assignment_unit_4.auth
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,12 +25,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.assignment_unit_4.ui.theme.Purple80
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,7 +54,10 @@ fun SignUp() {
   }
 
   Scaffold(topBar = {
-    TopAppBar(title = { Text(text = "Sign Up") })
+    TopAppBar(
+      title = { Text(text = "Sign Up") },
+      colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Purple80)
+    )
   }) { contentPadding ->
     Box(modifier = Modifier.padding(contentPadding)) {
       Column(
@@ -98,6 +106,10 @@ fun SignUp() {
           modifier = Modifier.fillMaxWidth(),
           keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(onClick = { /*TODO*/ }, modifier = Modifier.width(250.dp).height(50.dp)) {
+          Text(text = "Sign Up", fontSize = 20.sp)
+        }
       }
     }
   }

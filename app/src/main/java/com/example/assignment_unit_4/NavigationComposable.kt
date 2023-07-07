fe0b2fcebfabdb7814cf29498915674592b23d44
Kyaw Thu <evanch98@gmail.com>
2023-07-08 00:10:22 +0800

    initialize navigation

--- app/src/main/java/com/example/assignment_unit_4/NavigationComposable.kt
+++ app/src/main/java/com/example/assignment_unit_4/NavigationComposable.kt
@@ -1,2 +1,10 @@
 package com.example.assignment_unit_4
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+
+@Composable
+fun Navigation(navController: NavHostController) {
+
+}

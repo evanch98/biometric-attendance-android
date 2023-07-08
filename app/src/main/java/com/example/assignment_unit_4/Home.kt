87201e744cc76b8a02b2578fb12441da89b22e9f
Kyaw Thu <evanch98@gmail.com>
2023-07-08 12:12:32 +0800

    update the Home composable

--- app/src/main/java/com/example/assignment_unit_4/Home.kt
+++ app/src/main/java/com/example/assignment_unit_4/Home.kt
@@ -1,14 +1,20 @@
 package com.example.assignment_unit_4
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.assignment_unit_4.ui.theme.Purple80
 
@@ -22,7 +28,17 @@ fun Home(navController: NavController) {
     )
   }) { contentPadding ->
     Box(modifier = Modifier.padding(contentPadding)) {
-
+      Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(20.dp)
+      ) {
+        Button(onClick = { /*TODO*/ }) {
+          Text(text = "Check-in")
+        }
+      }
     }
   }
 }

6b51a33bf4653eb621c4337f07713307f060cf99
Kyaw Thu <evanch98@gmail.com>
2023-07-08 13:12:15 +0800

    initialize the Biometric object

--- app/src/main/java/com/example/assignment_unit_4/Biometric.kt
+++ app/src/main/java/com/example/assignment_unit_4/Biometric.kt
@@ -1,2 +1,10 @@
 package com.example.assignment_unit_4
 
+import android.content.Context
+import android.hardware.biometrics.BiometricManager
+
+object Biometric {
+  fun statusName(context: Context) {
+
+  }
+}

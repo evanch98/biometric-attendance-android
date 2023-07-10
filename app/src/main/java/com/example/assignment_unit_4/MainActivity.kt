93f004137b1223604084b63a1b2336a45d808d3a
Kyaw Thu <evanch98@gmail.com>
2023-07-11 00:39:50 +0800

    update the MainActivity

--- app/src/main/java/com/example/assignment_unit_4/MainActivity.kt
+++ app/src/main/java/com/example/assignment_unit_4/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.assignment_unit_4
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,53 +27,25 @@ class MainActivity : FragmentActivity() {
       .allowMainThreadQueries().build()
   }
 
-  private val requiredPermissions = arrayOf(
-    Manifest.permission.ACCESS_COARSE_LOCATION,
-    Manifest.permission.ACCESS_FINE_LOCATION
-  )
-
-  private val locationPermissionRequest = registerForActivityResult(
-    ActivityResultContracts.RequestMultiplePermissions()
-  ) { permissions ->
-    when {
-      requiredPermissions.all {
-        ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
-      } -> {
-        Toast.makeText(this, "Access granted", Toast.LENGTH_SHORT).show()
-      }
-
-      permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-        Toast.makeText(this, "Access granted", Toast.LENGTH_SHORT).show()
-      }
-
-      permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-        Toast.makeText(this, "Access granted", Toast.LENGTH_SHORT).show()
-      }
-
-      else -> {
-        Toast.makeText(this, "Access denied", Toast.LENGTH_SHORT).show()
-      }
-    }
-  }
-
+  @RequiresApi(Build.VERSION_CODES.O)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       Assignment_unit_4Theme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          MyNavigation(this@MainActivity, database, locationPermissionRequest)
+          MyNavigation(this@MainActivity, database)
         }
       }
     }
   }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MyNavigation(
   activity: FragmentActivity,
   database: AppDatabase,
-  locationPermissionRequest: ActivityResultLauncher<Array<String>>
 ) {
   val navController = rememberNavController()
   Navigation(

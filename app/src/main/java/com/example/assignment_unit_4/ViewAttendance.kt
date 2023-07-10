c2b034c74802c16e601e4d275bbef85a4e0bf6f0
Kyaw Thu <evanch98@gmail.com>
2023-07-11 00:17:04 +0800

    add a new composable ViewAttendance

--- app/src/main/java/com/example/assignment_unit_4/ViewAttendance.kt
+++ app/src/main/java/com/example/assignment_unit_4/ViewAttendance.kt
@@ -1,2 +1,9 @@
 package com.example.assignment_unit_4
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ViewAttendance(navController: NavController, database: AppDatabase) {
+
+}

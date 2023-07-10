836515619271d7f4daf398860d9ce97d8547fe50
Kyaw Thu <evanch98@gmail.com>
2023-07-10 23:20:30 +0800

    add a new utility function getCurrentTime

--- app/src/main/java/com/example/assignment_unit_4/utils/getCurrentTime.kt
+++ app/src/main/java/com/example/assignment_unit_4/utils/getCurrentTime.kt
@@ -1,2 +1,13 @@
 package com.example.assignment_unit_4.utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun getCurrentTime(): String {
+  val current = LocalDateTime.now()
+  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+  return current.format(formatter)
+}
